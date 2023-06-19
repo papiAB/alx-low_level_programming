82aef5413187dc97d8e43d430cfcfd0166f44542
papiAB <nanisa.3221@gmail.com>
2023-06-19 12:10:45 -0700

    advanced

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+/**
+*main - entry point
+*
+* Return: always returns 0
+*/
+int main(void)
+{
+int i, j;
+for (i = 0; j <= 99; j++)
+{
+for (i = j; i <= 99; i++)
+{
+if (i != j)
+{
+putchar(j / 10 + 48);
+putchar(j % 10 + 48);
+putchar(' ');
+putchar(i / 10 + 48);
+putchar(i % 10 + 48);
+if (j * 100 + i  != 9899)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
